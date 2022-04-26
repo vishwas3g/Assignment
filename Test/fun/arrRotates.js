991bb87fc98334bce4b72b7d9982e407162ae4bc
vishwas3g <60092787+vishwas3g@users.noreply.github.com>
2022-04-26 11:22:57 +0530

    Add files via upload

--- Test/fun/arrRotates.js
+++ Test/fun/arrRotates.js
@@ -5,3 +5,16 @@ function rotate(nums, k) {
     return nums;
 };
 rotate([1,2,3,4,5,6,7],3);
+
+
+function rotate(arr, rotations){
+    for(let i = 0; i< rotations ; i++){
+        let element = arr.pop();
+        console.log("POP",arr);
+        arr.unshift(element);
+        console.log("UNSHIFT",arr);
+    }
+    return arr;
+}
+
+rotate([1,2,3,4,5,6,7,8,9],2);
